d7aaf6511cc71d14abb2e7c7c72c2dbd1276db94
geraldywy <geraldywy@gmail.com>
2022-12-22 23:38:54 +0700

    2.4

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -9,21 +9,25 @@ import (
 
 type Lexer struct {
 	input    string
-	position int  // current position in input (points to current char
-	ch       byte // current char under examination
+	position int  // position in input to resume reading (also read as, not read in yet)
+	ch       byte // prev char read in
 
 	// metadata
-	fileName string
-	lineNum  int
-	linePos  int // line position is 1-indexed
+	FileName string
+	LineNum  int // line num is 1-indexed
+	LinePos  int // line position is 1-indexed
 }
 
 func New(input string, fileName string) *Lexer {
-	l := &Lexer{input: input, fileName: fileName}
+	l := &Lexer{input: input, FileName: fileName, LineNum: 1, LinePos: 1}
 	return l
 }
 
-func (l *Lexer) peekCurrent() byte {
+func (l *Lexer) Debug() (string, int, string) {
+	return string(l.ch), l.position, "->" + string(l.input[l.position]) + "<-"
+}
+
+func (l *Lexer) peekNext() byte {
 	if l.position == len(l.input) {
 		return 0
 	}
@@ -37,32 +41,39 @@ func (l *Lexer) readChar() {
 		return
 	}
 	if l.ch == '\n' {
-		l.lineNum++
-		l.linePos = 0
+		l.LineNum++
+		l.LinePos = 0
 	}
 	l.ch = l.input[l.position]
 	l.position++
-	l.linePos++
+	l.LinePos++
 }
 
-func (l *Lexer) NextToken() (*token.Token, error) {
-	l.eatWhitespaces()
+func (l *Lexer) byte2Token(ch byte, isPeek bool) (*token.Token, error) {
+	startPos := l.position
+	defer func() {
+		// restore for peeks
+		if isPeek {
+			l.position = startPos
+		}
+	}()
+
 	l.readChar()
-	if tt, exist := token.SingleToken[l.ch]; exist {
+	if tt, exist := token.SingleToken[ch]; exist {
 		// special case for double tokens
-		cand := string(l.ch) + string(l.peekCurrent())
+		cand := string(ch) + string(l.peekNext())
 		if dblTt, candExist := token.DoubleToken[cand]; candExist {
 			l.readChar()
 			return newToken(dblTt, cand), nil
 		}
 
-		return newToken(tt, string(l.ch)), nil
-	} else if l.ch == 0 {
+		return newToken(tt, string(ch)), nil
+	} else if ch == 0 {
 		return newToken(token.EOF, ""), nil
 	}
 
 	// handle all keywords/identifiers/numbers (really, just integers)
-	if isSupportedChar(l.ch) {
+	if isSupportedChar(ch) {
 		literal, err := l.readIdentLiteral()
 		if err != nil {
 			return nil, err
@@ -73,11 +84,21 @@ func (l *Lexer) NextToken() (*token.Token, error) {
 		}, nil
 	}
 
-	return newToken(token.ILLEGAL, string(l.ch)), nil
+	return newToken(token.ILLEGAL, string(ch)), nil
+}
+
+func (l *Lexer) NextToken() (*token.Token, error) {
+	l.eatWhitespaces()
+	return l.byte2Token(l.peekNext(), false)
+}
+
+func (l *Lexer) PeekToken() (*token.Token, error) {
+	l.eatWhitespaces()
+	return l.byte2Token(l.peekNext(), true)
 }
 
 func (l *Lexer) eatWhitespaces() {
-	for utils.IsWhitespace(l.peekCurrent()) {
+	for utils.IsWhitespace(l.peekNext()) {
 		l.readChar()
 	}
 }
@@ -85,16 +106,16 @@ func (l *Lexer) eatWhitespaces() {
 func (l *Lexer) readIdentLiteral() (string, error) {
 	start := l.position - 1
 	if utils.IsDigit(l.ch) { // is a number
-		for utils.IsDigit(l.peekCurrent()) {
+		for utils.IsDigit(l.peekNext()) {
 			l.readChar()
 		}
-		if utils.IsAlphaOrUnderscore(l.peekCurrent()) {
+		if utils.IsAlphaOrUnderscore(l.peekNext()) {
 			// a variable cannot start with a number
-			logger.PrettyPrintErr(l.fileName, l.lineNum, l.linePos, ErrBadVariableName)
+			logger.PrettyPrintErr(l.FileName, l.LineNum, l.LinePos, ErrBadVariableName)
 			return "", ErrBadVariableName
 		}
 	} else {
-		for utils.IsAlphaOrUnderscore(l.peekCurrent()) || utils.IsDigit(l.peekCurrent()) {
+		for utils.IsAlphaOrUnderscore(l.peekNext()) || utils.IsDigit(l.peekNext()) {
 			l.readChar()
 		}
 	}
---
+++ parser/parser.go
@@ -0,0 +1,104 @@
+package parser
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/geraldywy/monkey/ast"
+	"github.com/geraldywy/monkey/lexer"
+	"github.com/geraldywy/monkey/token"
+)
+
+type Parser struct {
+	l      *lexer.Lexer
+	Errors []error
+}
+
+func New(l *lexer.Lexer) *Parser {
+	p := &Parser{l: l}
+	return p
+}
+
+func (p *Parser) ParseProgram() *ast.Program {
+	prog := new(ast.Program)
+	prog.Statements = make([]ast.Statement, 0)
+	var tkn *token.Token
+	var peekErr error
+
+	for tkn, peekErr = p.l.NextToken(); tkn.Type != token.EOF; tkn, peekErr = p.l.NextToken() {
+		if peekErr != nil {
+			p.Errors = append(p.Errors, peekErr)
+			break
+		}
+		stmt, err := p.parseStatement(tkn)
+		if err != nil {
+			p.Errors = append(p.Errors, err)
+			continue // redundant, but just leaving it in here for clarity
+		}
+		if stmt != nil {
+			prog.Statements = append(prog.Statements, stmt)
+		}
+	}
+
+	return prog
+}
+
+func (p *Parser) parseStatement(startToken *token.Token) (ast.Statement, error) {
+	switch startToken.Type {
+	case token.LET:
+		return p.parseLetStatement(startToken)
+	default:
+		return nil, nil
+	}
+}
+
+func (p *Parser) parseLetStatement(startToken *token.Token) (*ast.LetStatement, error) {
+	stmt := &ast.LetStatement{
+		Token: startToken,
+	}
+	if err := p.assertPeek(token.IDENT); err != nil {
+		return nil, err
+	}
+
+	nameToken, err := p.l.NextToken()
+	if err != nil {
+		return nil, err
+	}
+	stmt.Name = &ast.Identifier{
+		Token: nameToken,
+		Value: nameToken.Literal,
+	}
+
+	if err := p.assertPeek(token.ASSIGN); err != nil {
+		return nil, err
+	}
+	if _, err := p.l.NextToken(); err != nil {
+		return nil, err
+	}
+
+	// TODO, skip value
+	for err := p.assertPeek(token.SEMICOLON); err != nil; _, err = p.l.NextToken() {
+	}
+
+	return stmt, nil
+}
+
+func (p *Parser) assertPeek(wantTkn token.TokenType) error {
+	tkn, err := p.l.PeekToken()
+	if err != nil {
+		return err
+	}
+
+	if tkn.Type != wantTkn {
+		return errors.New(fmt.Sprintf(
+			"%s line: %d col: %d expected token: %s, got %s",
+			p.l.FileName,
+			p.l.LineNum,
+			p.l.LinePos,
+			wantTkn,
+			tkn.Type,
+		))
+	}
+
+	return nil
+}
