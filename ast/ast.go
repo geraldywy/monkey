3303bf6444017b91d3e58c70d88f16d48662a700
geraldywy <geraldywy@gmail.com>
2022-12-24 19:12:04 +0700

    2.6-wip

--- ast/ast.go
+++ ast/ast.go
@@ -1,10 +1,17 @@
 package ast
 
-import "github.com/geraldywy/monkey/token"
+import (
+	"bytes"
+	"strings"
+
+	"github.com/geraldywy/monkey/token"
+)
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
+
 type Statement interface {
 	Node
 	statementNode()
@@ -26,6 +33,16 @@ func (p *Program) TokenLiteral() string {
 	return p.Statements[0].TokenLiteral()
 }
 
+func (p *Program) String() string {
+	var sb strings.Builder
+
+	for _, s := range p.Statements {
+		sb.WriteString(s.String())
+	}
+
+	return sb.String()
+}
+
 type LetStatement struct {
 	Token *token.Token // the token.LET token
 	Name  *Identifier
@@ -42,6 +59,7 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string       { return i.Value }
 
 type ReturnStatement struct {
 	Token       *token.Token // the 'return' token
@@ -50,3 +68,49 @@ type ReturnStatement struct {
 
 func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+
+type ExpressionStatement struct {
+	Token      *token.Token // the first token of the expression
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
+
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
+}
+
+type IntegerLiteral struct {
+	Token *token.Token
+	Value int64
+}
+
+func (il *IntegerLiteral) expressionNode()      {}
+func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
+func (il *IntegerLiteral) String() string       { return il.Token.Literal }
--- parser/parser.go
+++ parser/parser.go
@@ -3,20 +3,62 @@ package parser
 import (
 	"errors"
 	"fmt"
+	"strconv"
 
 	"github.com/geraldywy/monkey/ast"
 	"github.com/geraldywy/monkey/lexer"
 	"github.com/geraldywy/monkey/token"
 )
 
+const (
+	_ int = iota
+	LOWEST
+	EQUALS      // ==
+	LESSGREATER // > or <
+	SUM         //+
+	PRODUCT     //*
+	PREFIX      //-Xor!X
+	CALL        // myFunction(X)
+)
+
 type Parser struct {
 	l      *lexer.Lexer
 	Errors []error
+
+	prefixParseFns map[token.TokenType]prefixParseFn
+	infixParseFns  map[token.TokenType]infixParseFn
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l: l,
+		prefixParseFns: map[token.TokenType]prefixParseFn{
+			token.IDENT: parseIdentifier,
+			token.INT:   parseIntegerLiteral,
+		},
+	}
+
 	return p
+}
+
+func parseIdentifier(tkn *token.Token) (ast.Expression, error) {
+	return &ast.Identifier{
+		Token: tkn,
+		Value: tkn.Literal,
+	}, nil
+}
+
+func parseIntegerLiteral(tkn *token.Token) (ast.Expression, error) {
+	exp := &ast.IntegerLiteral{
+		Token: tkn,
+	}
+	var err error
+	exp.Value, err = strconv.ParseInt(tkn.Literal, 10, 64)
+	if err != nil {
+		return nil, err
+	}
+
+	return exp, nil
 }
 
 func (p *Parser) ParseProgram() *ast.Program {
@@ -50,7 +92,7 @@ func (p *Parser) parseStatement(startToken *token.Token) (ast.Statement, error) 
 	case token.RETURN:
 		return p.parseReturnStatement(startToken)
 	default:
-		return nil, nil
+		return p.parseExpressionStatement(startToken)
 	}
 }
 
@@ -81,6 +123,10 @@ func (p *Parser) parseLetStatement(startToken *token.Token) (*ast.LetStatement, 
 	// TODO, skip value
 	for err := p.assertPeek(token.SEMICOLON); err != nil; _, err = p.l.NextToken() {
 	}
+	if err := p.assertPeek(token.SEMICOLON); err != nil {
+		return nil, err
+	}
+	p.l.NextToken() // skip semi
 
 	return stmt, nil
 }
@@ -93,8 +139,39 @@ func (p *Parser) parseReturnStatement(startToken *token.Token) (*ast.ReturnState
 	// TODO, skip value
 	for err := p.assertPeek(token.SEMICOLON); err != nil; _, err = p.l.NextToken() {
 	}
+	if err := p.assertPeek(token.SEMICOLON); err != nil {
+		return nil, err
+	}
+	p.l.NextToken() // skip semi
 
 	return stmt, nil
+}
+
+func (p *Parser) parseExpressionStatement(startToken *token.Token) (*ast.ExpressionStatement, error) {
+	exp, err := p.parseExpression(startToken, LOWEST)
+	if err != nil {
+		p.Errors = append(p.Errors, err)
+		return nil, err
+	}
+	stmt := &ast.ExpressionStatement{
+		Token:      startToken,
+		Expression: exp,
+	}
+
+	// advance if is semicolon
+	if err := p.assertPeek(token.SEMICOLON); err == nil {
+		p.l.NextToken()
+	}
+
+	return stmt, nil
+}
+
+func (p *Parser) parseExpression(startToken *token.Token, precedence int) (ast.Expression, error) {
+	if prefix, exist := p.prefixParseFns[startToken.Type]; exist {
+		return prefix(startToken)
+	}
+
+	return nil, nil
 }
 
 func (p *Parser) assertPeek(wantTkn token.TokenType) error {
@@ -115,4 +192,17 @@ func (p *Parser) assertPeek(wantTkn token.TokenType) error {
 	}
 
 	return nil
+}
+
+type (
+	prefixParseFn func(tkn *token.Token) (ast.Expression, error)
+	infixParseFn  func(expression ast.Expression) (ast.Expression, error)
+)
+
+func (p *Parser) registerPrefix(tokenType token.TokenType, fn prefixParseFn) {
+	p.prefixParseFns[tokenType] = fn
+}
+
+func (p *Parser) registerInfix(tokenType token.TokenType, fn infixParseFn) {
+	p.infixParseFns[tokenType] = fn
 }
